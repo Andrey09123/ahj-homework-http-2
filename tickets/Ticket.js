335375688d37aa0ee2e5d74111ee7a3440905f1c
Andre Arkhipov <andre.arhipow@gmail.com>
2023-05-10 13:50:13 +0300

    commit

---
+++ tickets/Ticket.js
@@ -0,0 +1,23 @@
+const getCreated= require('./getCreated'); 
+
+class Ticket {
+    constructor(data) {
+      this.id = Date.now();
+      this.name = data.name;
+      this.status = data.status,
+      this.created = getCreated();
+      this.description = description,
+    }
+
+    getData() {
+      return {
+        id: this.id,
+        name: this.name,
+        status: this.status,
+        created: this.created,
+        description: this.description,
+      }
+    }
+}
+
+module.exports = Ticket;
